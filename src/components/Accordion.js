57055302c92f3265f664a0c25457c6384056e0a4
briandizon <bvdizon@gmail.com>
2021-01-19 15:47:08 +0800

    minor updates

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
 const Accordion = ({ id, title, info }) => {
+  // state to control the show and hide of accordion content
   const [state, setstate] = useState(false);
 
   return (
     <div className='accordion' onClick={() => setstate(!state)}>
       <header
+        // Conditionally styling based if accordion is open or close
         style={{
           borderBottom: state
             ? '1px solid blueviolet'
@@ -15,6 +17,7 @@ const Accordion = ({ id, title, info }) => {
         <h3>{title}</h3>
         <span>{state ? <AiOutlineMinus /> : <AiOutlinePlus />}</span>
       </header>
+      {/* Conditional Render using Short-circuit Evaluation  */}
       <p>{state && info}</p>
     </div>
   );
